65430905ce87ab71d058ab03a4542ddc87a65f78
Le Duy Vu <leduyvupro123@gmail.com>
2026-03-10 13:22:03 +0700

    Bai 6

--- BaitapJava/BaitapJava/src/main/java/com/example/BaitapJava/Bai6/controller/ProductController.java
+++ BaitapJava/BaitapJava/src/main/java/com/example/BaitapJava/Bai6/controller/ProductController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/products")
---
+++ BaitapJava/BaitapJava/src/main/java/com/example/BaitapJava/Bai6/model/Role.java
@@ -0,0 +1,14 @@
+package com.example.BaitapJava.Bai4.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+}
